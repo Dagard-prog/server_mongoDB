83259902a22ea6b1494e4070436ba91a8ed67b69
Dagard-prog <daniel.64.a@gmail.com>
2020-03-28 22:15:28 -0600

    actualización

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 const Task = require('../models/task');
 
-router.get('/', (req, res) => {
-    res.render('index')
+router.get('/', async (req, res) => {
+
+    const tasks = await Task.find();
+    console.log(tasks);
+    res.render('index',{
+
+        tasks
+
+    });
 
 });
 
