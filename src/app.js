87a3a8e38d8928683c627aa9d598c2bf8e4d9cfb
Dagard-prog <daniel.64.a@gmail.com>
2020-03-28 21:38:57 -0600

    Holi bb

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+
+//connecting to db 
+mongoose.connect('mongodb://localhost/crud-mongo', {useNewUrlParser: true,useUnifiedTopology: true})
+    .then(db => console.log('DB connect'))
+    .catch(err => console.log(err));
+
+
+
+//importing routes
+const indexRoutes= require('./routes/index');
+
+//settings 
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//middlewares 
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended : false}));
+
+//routes
+app.use('/', indexRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//starting de server
+app.listen(app.get('port'), () => {
+
+    console.log(`Server on port ${app.get('port')}`);
+})
